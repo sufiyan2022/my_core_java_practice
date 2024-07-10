31d27089bc4bf28e28583e2f00d4fe9385709c37
sufiyan2022 <100289395+sufiyan2022@users.noreply.github.com>
2024-07-10 22:26:17 +0530

    SCP

---
+++ core_java_mypractice/src/com/ras/practice/string/Test2.java
@@ -0,0 +1,15 @@
+package com.ras.practice.string;
+
+public class Test2 {
+public static void main(String[] args) {
+	
+	String s = "Prakash";
+	
+	/*
+	 *  heap - 0
+	 *  SCP -  1
+	 *  Total- 1 
+	 * 
+	 */
+}
+}
