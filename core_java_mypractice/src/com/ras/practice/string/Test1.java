917ddd44fb0825ee0915b381e86cb79c6f0476c3
sufiyan2022 <100289395+sufiyan2022@users.noreply.github.com>
2024-07-10 22:19:55 +0530

    String objects

---
+++ core_java_mypractice/src/com/ras/practice/string/Test1.java
@@ -0,0 +1,16 @@
+package com.ras.practice.string;
+
+public class Test1 {
+public static void main(String[] args) {
+	
+	String s = new String("prakash");
+	System.out.println(s);
+	
+	/*
+	 *  heap -1
+	 *  scp - 1
+	 *  total-2
+	 * 
+	 */
+}
+}
